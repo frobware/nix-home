65381062214d25a6da2936c4b26c4231c4a949ac
Andrew McDermott <aim@frobware.com>
2020-11-23 20:33:09 +0000

    xdwim updates

--- pkgs/xdwim/default.nix
+++ pkgs/xdwim/default.nix
@@ -2,7 +2,7 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "xdwim";
-  version = "8724b19f3e56bb458fa0d4ecd6959edd35792b0f";
+  version = "master";
 
   buildInputs = with pkgs; [
     binutils
@@ -23,5 +23,5 @@ rustPlatform.buildRustPackage rec {
     sha256 = "sha256:1imqaygr24ndilrrgsd87kl64n2baiwx0f4nh618bmy88c4cvcl7";
   };
 
-  cargoSha256 = "sha256:0bzv6sfl3g9r4qnrvjdqskhpwq4d8nl2f4jxrpqmiv286f2nw4f5";
+  cargoSha256 = "sha256:1dansavnpp61mc65lbr4jbi45y6r0yki7h8xna42gxhxw5hsk9g9";
 }
