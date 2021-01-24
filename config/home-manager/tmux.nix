0ca624a40ead0fb0f7bbcdd99b1d05785fee0e9c
Andrew McDermott <aim@frobware.com>
2021-01-24 15:49:59 +0000

    Move tmux.conf into dotfiles

--- config/home-manager/tmux.nix
+++ config/home-manager/tmux.nix
@@ -1,11 +1,11 @@
 { config, lib, pkgs, ... }:
 
 {
-  xdg.configFile."tmux/tmux.conf".source = ../../home/programs/tmux.conf;
+  xdg.configFile."tmux/tmux.conf".source = ../../dotfiles/tmux.conf;
 
   programs.tmux = {
     enable = true;
-    extraConfig = builtins.readFile ../../home/programs/tmux.conf;
+    extraConfig = builtins.readFile ../../dotfiles/tmux.conf;
     plugins = with pkgs; [{
       plugin = tmuxPlugins.sidebar;
       extraConfig = "set -g @sidebar-tree-command 'tree -C'";
